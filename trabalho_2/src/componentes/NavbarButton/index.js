81cc0563bd6fe2ce8528bfca19a8030257e06205
Nickolas Crema <nick.cremaa@gmail.com>
2023-07-06 17:17:57 -0300

    add Routes to NavBarButtons

--- trabalho_2/src/componentes/NavbarButton/index.js
+++ trabalho_2/src/componentes/NavbarButton/index.js
@@ -1,14 +1,19 @@
 import './NavbarButton.css';
+import { useNavigate } from 'react-router-dom';
 
 
 const NavbarButton = (props) => {
+    const navigate = useNavigate();
+    const navigateToRegister = (routePath) => {
+        navigate(routePath)
+    }
     return (
-        // <div className='div'>
-            <button className='button'>
+        <div>
+            <button className='button' onClick={() => navigateToRegister(props.routePath)}>
                 <span>{props.icon}</span>
                 <span className='span-text'>{props.texto}</span>
             </button>
-        // </div>
+        </div>
     )
 }
 
