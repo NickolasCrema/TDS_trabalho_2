7a55b2246707a71ef2f6b296b0cb383fe850c6dd
Nickolas Crema <50722116+NickolasCrema@users.noreply.github.com>
2023-07-06 23:58:11 -0300

    update

--- trabalho_2/src/componentes/ComplementoFormularioCadastro/index.js
+++ trabalho_2/src/componentes/ComplementoFormularioCadastro/index.js
@@ -25,38 +25,38 @@ const ComplementoFormularioCadastro = () => {
         <div className="complemento-formulario-cadastro">
 
             <div className='block'>
-                <div className='display-inline display-50'>
+                <div className='display-inline'>
                     <CampoTextoAutoFill label='E-mail' placeholder='Digite seu e-mail' valor={email}/>
                 </div>
-                <div className='display-inline display-50'>
+                <div className='display-inline'>
                     <CampoTexto label='Senha' placeholder='Digite sua senha' type='password'/>
                 </div>
             </div>
 
             <div className='block'>
-                <div className='display-inline display-50'>
+                <div className='display-inline'>
                     <CampoCEP label='CEP' placeholder='Digite seu CEP' change={setInfos}/>
 
                 </div>
-                <div className='display-inline display-50'>
+                <div className='display-inline'>
                     <CampoTextoAutoFill label='Logradouro' placeholder='Digite seu logradouro' valor={endereco}/>
                 </div>
             </div>
 
             <div className='block'>
-                <div className='display-inline display-30'>
+                <div className='display-inline'>
                     <CampoTexto label='Número' placeholder='Digite o número de sua residência' type='text'/>
                 </div>
-                <div className='display-inline display-70'>
+                <div className='display-inline'>
                     <CampoTextoAutoFill label='Bairro' placeholder='Digite seu bairro' valor={bairro}/>
                 </div>
             </div>
 
             <div className='block'>
-                <div className='display-inline display-50'>
+                <div className='display-inline'>
                     <CampoTextoAutoFill label='Cidade' placeholder='Digite sua Cidade' valor={cidade}/>
                 </div>
-                <div className='display-inline display-50'>
+                <div className='display-inline'>
                     <CampoTextoAutoFill label='Estado' placeholder='Digite seu estado' valor={uf}/>
                 </div>
             </div>
